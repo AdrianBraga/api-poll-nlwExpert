95dd781138a3f20b3e76edfb647d8ed85e5ce9c1
Adrian Braga <adrian.braga.ro@gmail.com>
2024-04-13 01:15:00 -0400

    Create routes list Polls

---
+++ src/http/routes/get-poll.ts
@@ -0,0 +1,30 @@
+import { FastifyInstance } from "fastify";
+import { z } from 'zod';
+import { prisma } from "../../lib/prisma";
+
+
+export async function GetPoll(app: FastifyInstance) {
+  app.get('/polls/:id', async (request, reply) => {
+    const getPollParams = z.object({
+      id: z.string(),
+    });
+  
+    const { id } = getPollParams.parse(request.params);
+
+    const poll = await prisma.poll.findUnique({
+      where: {
+        id,
+      },
+      include: {
+        options: {
+          select: {
+            id: true,
+            title: true
+          }
+        }
+      }
+    })
+  
+    return reply.send({ poll });
+  })
+}
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,11 @@
 import fastify from 'fastify';
 import { CreatePoll } from './routes/create-poll';
+import { GetPoll } from './routes/get-poll';
 
 const app = fastify();
 
 app.register(CreatePoll);
+app.register(GetPoll);
 
 app.listen({
   port: 3333
